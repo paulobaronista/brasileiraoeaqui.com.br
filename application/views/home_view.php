a505ee671e8f662fe92ce0022664aae8619d1328
PauloBaronista <paulo@rpm.com.br>
2020-08-31 16:32:55 -0300

    limpando codigo

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,33 +1,28 @@
-<div class="container-fluid home padding-off-mobile">
-    <div class="hidden-xs col-xs-12 col-sm-7 col-md-7 col-lg-8"></div>
-    <div class="col-xs-12 col-sm-5 col-md-5 col-lg-4">
-        <div class="col-xs-12 col-sm-12 col-md-12 padding-off form">
-            <form method="post" role="form" action="<?php echo base_url("contato")?>">
-                <span class="tt-form text-center">
-                    <p>Preencha os dados abaixo para<br/> um atendimento personalizado!</p>
-                </span>
-                <div class="group-form">
-                    <div class="form-group">
-                        <label for="nome">Nome*</label>
-                        <input id="nome" type="text" class="nome form-control" name="nome" required="required" />
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email*</label>
-                        <input id="email" type="email" class="email form-control" name="email" required="required"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="telefone">Telefone</label>
-                        <input id="telefone" class="phone form-control" type="tel" name="phone"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="form-mensagem">Mensagem</label>
-                        <textarea id="form-mensagem" class="msg form-control" rows="3" name="mss"></textarea>
-                    </div>
-                    <button type="submit" class="btn_enviar enviar btn center-block" title="enviar" name="enviar_email" value="enviar">Enviar</button>
-                </div>
-            </form>
+<div class="col-xs-12 col-sm-12 col-md-12">
+    <form method="post" role="form" action="<?php echo base_url("contato") ?>">
+        <span class="tt-form text-center">
+            <p>Preencha os dados abaixo para<br /> um atendimento personalizado!</p>
+        </span>
+        <div class="group-form">
+            <div class="form-group">
+                <label for="nome">Nome*</label>
+                <input id="nome" type="text" class="nome form-control" name="nome" required="required" />
+            </div>
+            <div class="form-group">
+                <label for="email">Email*</label>
+                <input id="email" type="email" class="email form-control" name="email" required="required" />
+            </div>
+            <div class="form-group">
+                <label for="telefone">Telefone</label>
+                <input id="telefone" class="phone form-control" type="tel" name="phone" />
+            </div>
+            <div class="form-group">
+                <label for="form-mensagem">Mensagem</label>
+                <textarea id="form-mensagem" class="msg form-control" rows="3" name="mss"></textarea>
+            </div>
+            <button type="submit" class="btn_enviar enviar btn center-block" title="enviar" name="enviar_email" value="enviar">Enviar</button>
         </div>
-    </div>
+    </form>
 </div>
 
 <footer>
@@ -39,7 +34,8 @@
         </div>
     </div>
 </footer>
+
 <?php
-if(isset($email_enviado))
+if (isset($email_enviado))
     echo $email_enviado;
 ?>
